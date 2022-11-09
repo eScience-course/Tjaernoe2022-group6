9fc3f4a7814b11b23b790768fafb1b933950a168
annegw <annegrow1698@outlook.com>
2022-11-09 14:53:56 +0000

    sende filer

--- notebooks/Annegro/myfuctions.py
+++ notebooks/Annegro/myfuctions.py
@@ -1,19 +1,18 @@
-def plotmap(): 
-    ax1.contourf(dr_out.lon,
-              dr_out.lat,
-              dr_out.isel(season=),
-              levels=np.arange(0, 101, 5),
+import cartopy.crs as ccrs
+
+
+def plotmap(ax,dr,season, levels):
+    cf=ax.contourf(dr.lon,
+              dr.lat,
+              dr.sel(season=season),
+              levels=levels,
               transform=ccrs.PlateCarree(),
                  
                  
               )
-    
-    f.suptitle('H')    
-  
-f.colorbar(W , label='sea ice concentration [%]') #, ax=ax1)
-ax1.set_extent([-180, 180, 90, 50], ccrs.PlateCarree())
-ax1.set_title('DJF')
-ax1.gridlines(draw_labels=True) 
-ax1.coastlines()
-plt.tight_layout()
+    ax.set_extent([-180, 180, 90, 50], ccrs.PlateCarree())
+    ax.set_title(season)
+    ax.gridlines(draw_labels=True) 
+    ax.coastlines()
+    return cf 
     
